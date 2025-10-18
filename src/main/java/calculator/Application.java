c6ec2dec56ffd92821dc833d8b61d335064affe1
sejung2 <seingdev@gmail.com>
2025-10-19 00:18:16 +0900

    feat: 입력값 구분 완료
    기본 구분자로 구분하기 완료
    커스텀 구분자로 구분하기 완료

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -9,5 +9,22 @@ public class Application {
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String inputString = readLine();
 
+        String custom = "";
+        String regex = ",|:";
+        String[] nums;
+
+        // 커스텀 구분자 유무 확인
+        if (inputString.startsWith("//")) {
+            int index = inputString.indexOf("\\n");
+            // 커스텀 구분자 추출
+            custom = inputString.substring(2, index);
+            inputString = inputString.substring(index + 2);
+
+            regex = regex + "|" + "\\\\" + custom;
+
+            nums = inputString.split(regex);
+        } else {
+            nums = inputString.split(regex);
+        }
     }
 }
